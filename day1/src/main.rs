7322d4a05f34f5ac0219e3c52ca122d03c1cbb9d
steve <ssosik@akamai.com>
2021-12-02 08:28:09 -0500

    Day 1 done: 1139

---
+++ day1/src/main.rs
@@ -0,0 +1,25 @@
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let input = BufReader::new(std::io::stdin());
+    let mut lines = input.lines();
+
+    let mut last: Option<usize> = None;
+    let mut cnt = 0;
+    for line in lines {
+        // Get the next values for a and b, rebased to zero and ordered
+        let a: Vec<usize> = line
+            .unwrap()
+            .split_whitespace()
+            .filter_map(|v| v.parse::<usize>().ok())
+            .collect();
+        let a = a[0];
+
+        if last.is_some() && a > last.unwrap() {
+            cnt += 1;
+        }
+
+        last = Some(a);
+    }
+    println!("{}", cnt);
+}
