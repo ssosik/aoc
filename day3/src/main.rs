7b1e453998de5bf65e42ad2f2e836cf98040fff5
steve <ssosik@akamai.com>
2021-12-03 10:16:05 -0500

    Day3 WIP

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,26 +4,25 @@ use std::error;
 
 // Change the alias to `Box<error::Error>`.
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+const RADIX: u32 = 10;
 
 fn main() -> Result<()> {
-    const RADIX: u32 = 10;
     
     let input = BufReader::new(std::io::stdin());
     let lines = input.lines();
 
     let mut bit_counts: BTreeMap<usize, BTreeMap<u8, usize>> = BTreeMap::new();
     for line in lines {
-        for (i, b) in line.unwrap().chars().enumerate() {
-            let b = b.to_digit(RADIX).expect("to digit failed") as u8;
-            bit_counts.entry(i)
+        for (idx, bit) in line.unwrap().chars().enumerate() {
+            let bit = bit.to_digit(RADIX).expect("to digit failed") as u8;
+            bit_counts.entry(idx)
                 .and_modify(|m| {
-                    m.entry(b)
-                    .and_modify(|x| *x += 1)
-                    //.or_insert(BTreeMap::from([(b, 1)]));
+                    m.entry(bit)
+                    .and_modify(|cnt| *cnt += 1)
                     .or_insert(1);
                 })
-                .or_insert(BTreeMap::from([(b, 1)]));
-            print!("{}:{} ", i, b);
+                .or_insert(BTreeMap::from([(bit, 1)]));
+            print!("{}:{} ", idx, bit);
         }
         println!("");
     };
