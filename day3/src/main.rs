a052bd8539f8b7500d2213a979db44f9a8e34a95
steve <ssosik@akamai.com>
2021-12-03 12:46:03 -0500

    WIP

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 use std::error;
 use std::io::{BufRead, BufReader};
 
@@ -29,16 +29,62 @@ fn main() -> Result<()> {
     let mut epsilon_rate = String::from("");
     let mut bit_counts: BTreeMap<usize, BTreeMap<u8, usize>> = BTreeMap::new();
 
+    let mut bit_idx = 0;
+    let mut msb1_mask = 31;
     while !&numbers.is_empty() {
-        let bit_count: BTreeMap<u8, usize> = BTreeMap::new();
+        // Find the larger bit count for the current bit_idx
+        let mut cnt0: usize = 0;
+        let mut cnt1: usize = 0;
         for n in &numbers {
             let bin_str = format!("{:05b}", n);
             print!("{} ", bin_str);
-            let msb = bin_str.chars().next().unwrap();
-            println!("MSB {}", msb);
-
+            match bin_str.chars().collect::<Vec<char>>()[bit_idx].to_digit(2).unwrap() {
+                0 => cnt0 += 1,
+                1 => cnt1 += 1,
+                _ => unreachable!(),
+            };
         }
-        numbers = vec![];
+        println!("");
+        msb1_mask = if cnt0 > cnt1 {
+            msb1_mask | (0 << (4 - bit_idx))
+        } else {
+            msb1_mask | (1 << (4 - bit_idx))
+        };
+
+        println!("Mask: {:05b} ({})", msb1_mask, msb1_mask);
+
+        let mut to_filter: BTreeSet<usize> = BTreeSet::new();
+        for n in &numbers {
+            to_filter.insert(*n);
+        }
+
+        for n in &numbers {
+
+            let masked1 = n & msb1_mask;
+            if masked1 == 0 {
+                to_filter.remove(n);
+            }
+            println!("CNT1 Mask {} {} {:05b}", n, masked1, msb1_mask);
+        }
+
+        println!("filtered {:?}", to_filter);
+        numbers = to_filter.iter().map(|x| *x).collect();
+
+
+        //match cnt0 > cnt1 {
+        //    true => {
+        //       println!("cnt0: {} {}", cnt0, 1 << (4 - bit_idx));
+        //    },
+        //    false => {
+        //       println!("cnt1: {} {}", cnt1, 1 << (4 - bit_idx));
+        //    },
+        //};
+        //println!("cnt0: {}, 1:{}, 2:{}, 3:{}, 4:{}, 5:{}",
+        //    cnt0, cnt0 << 1, cnt0 << 2, cnt0 << 3, cnt0 << 4, cnt0 << 5);
+        //println!("cnt1: {}, 1:{}, 2:{}, 3:{}, 4:{}, 5:{}",
+        //    cnt1, cnt1 << 1, cnt1 << 2, cnt1 << 3, cnt1 << 4, cnt1 << 5);
+        //numbers = vec![];
+        bit_idx += 1;
     }
 
     //for line in numbers.clone() {
