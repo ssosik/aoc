f19e253e7fb8fce7df5ea201571f841d4be52ec2
steve <ssosik@akamai.com>
2021-12-03 10:40:08 -0500

    Day3 Part1 correct answer

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -11,7 +11,10 @@ fn main() -> Result<()> {
     let input = BufReader::new(std::io::stdin());
     let lines = input.lines();
 
+    let mut gamma_rate = String::from("");
+    let mut epsilon_rate = String::from("");
     let mut bit_counts: BTreeMap<usize, BTreeMap<u8, usize>> = BTreeMap::new();
+
     for line in lines {
         for (idx, bit) in line.unwrap().chars().enumerate() {
             let bit = bit.to_digit(RADIX).expect("to digit failed") as u8;
@@ -22,11 +25,28 @@ fn main() -> Result<()> {
                     .or_insert(1);
                 })
                 .or_insert(BTreeMap::from([(bit, 1)]));
-            print!("{}:{} ", idx, bit);
+            //print!("{}:{} ", idx, bit);
         }
-        println!("");
+    };
+
+    for i in bit_counts.keys() {
+        match bit_counts[i][&0] > bit_counts[i][&1] {
+            true => {
+                gamma_rate.push_str("0");
+                epsilon_rate.push_str("1");
+            },
+            false => {
+                gamma_rate.push_str("1");
+                epsilon_rate.push_str("0");
+            },
+        };
     };
 
     println!("{:?}", bit_counts);
+    println!("Gamma:{} Epsilon:{}", gamma_rate, epsilon_rate);
+    let gamma_rate = isize::from_str_radix(gamma_rate.as_str(), 2).unwrap();
+    let epsilon_rate = isize::from_str_radix(epsilon_rate.as_str(), 2).unwrap();
+    println!("Gamma:{} Epsilon:{}", gamma_rate, epsilon_rate);
+    println!("Ans:{}", gamma_rate * epsilon_rate);
     Ok(())
 }
