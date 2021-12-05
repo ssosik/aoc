ce76245ba542fdc719fe5afa192ebfd1ac2993ea
steve <ssosik@akamai.com>
2021-12-04 20:31:00 -0500

    Parsing into array2d

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -28,57 +28,19 @@ fn main() -> Result<()> {
     println!("{:?}", marks);
 
     for chunk in lines.collect::<Vec<_>>().chunks(6) {
-        //println!("{:?} {:?}", chunk, print_type_of(chunk));
-        //println!("{:?}", &chunk[1..]);
-        let it = &(chunk[..])
+        let card = Array2D::from_rows(&(chunk[..])
             .iter()
             .skip(1)
-            .map(|x| {
-                x.as_ref()
+            .map(|row| {
+                row.as_ref()
                     .unwrap()
                     .split_whitespace()
                     .filter_map(|n| n.parse::<usize>().ok())
                     .collect::<Vec<_>>()
             })
-            .collect::<Vec<_>>();
-        println!("{:?}", it);
-        //let it = chunk.skip(1).map(|row| row.unwrap()).collect();
-        //println!("{:?}", chunk[1..]);
+            .collect::<Vec<_>>());
+        println!("{:?}", card);
     }
-
-    //let card: Array2D<usize>;
-    //lines.next();
-    //let rows: Vec<Vec<&str>> = lines.take(5).collect();
-    //println!("5 rows{:?}", rows);
-
-    //lines.skip(1).take(5).inspect(|x| println!("{:?}", x)).collect::<Vec<_>>();
-    //loop {
-    //    lines.by_ref()
-    //        .skip(1)
-    //        .take(5)
-    //        .inspect(|x| println!("x{:?}", x))
-    //        .collect::<Vec<_>>();
-    //        //.map(|x| x.unwrap().split_whitespace()
-    //        //    .filter_map(|v| v.parse::<usize>().ok()));
-    //    if lines.by_ref().is_empty() {
-    //        break;
-    //        }
-    //    //if lines.by_ref().peekable().peek().is_none() {
-    //    //    break;
-    //    //}
-    //}
-
-    //for line in lines {
-    //    let line: Vec<usize> = line
-    //        .unwrap()
-    //        .split_whitespace()
-    //        .filter_map(|v| v.parse::<usize>().ok())
-    //        .collect();
-    //    if line.len() == 0 {
-    //        
-    //    }
-    //    println!("{:?}", line);
-    //}
 
     Ok(())
 }
