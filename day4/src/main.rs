09e8c8af5d9c1046f8d5a96494c6d8b53b4b9862
steve <ssosik@akamai.com>
2021-12-04 21:08:25 -0500

    WIP

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,12 +1,8 @@
-use std::io::{BufRead, BufReader};
-use std::error;
 use array2d::Array2D;
+use std::error;
+use std::io::{BufRead, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
-#[derive(Debug)]
-struct Card {
-    rows: [[i32; 5]; 5],
-}
 
 //impl Card {
 //    fn new() -> Submarine {
@@ -21,28 +17,44 @@ fn print_type_of<T: ?Sized>(_: &T) {
 fn main() -> Result<()> {
     let input = BufReader::new(std::io::stdin());
     let mut lines = input.lines();
-    let marks: Vec<usize> = lines.next().unwrap().unwrap()
-            .split_terminator(",")
-            .filter_map(|v| v.parse::<usize>().ok())
-            .collect();
+    let marks: Vec<usize> = lines
+        .next()
+        .unwrap()
+        .unwrap()
+        .split_terminator(',')
+        .filter_map(|v| v.parse::<usize>().ok())
+        .collect();
     println!("{:?}", marks);
 
     let mut cards: Vec<Array2D<Option<usize>>> = Vec::new();
     for chunk in lines.collect::<Vec<_>>().chunks(6) {
-        let card = Array2D::from_rows(&(chunk[..])
-            .iter()
-            .skip(1)
-            .map(|row| {
-                row.as_ref()
-                    .unwrap()
-                    .split_whitespace()
-                    .filter_map(|n| Some(n.parse::<usize>().ok()))
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>());
+        let card = Array2D::from_rows(
+            &(chunk[..])
+                .iter()
+                .skip(1)
+                .map(|row| {
+                    row.as_ref()
+                        .unwrap()
+                        .split_whitespace()
+                        .filter_map(|n| Some(n.parse::<usize>().ok()))
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>(),
+        );
         cards.push(card);
     }
     println!("{:?}", cards);
 
+    let n = 29;
+    for mut card in cards {
+        for (i, row) in card.clone().rows_iter().enumerate() {
+            for (j, val) in row.enumerate() {
+                if val.is_some() && n == val.unwrap() {
+                    card.set(i, j, None);
+                    println!("bingo! {} {} {}", n, i, j);
+                }
+            }
+        }
+    }
     Ok(())
 }
