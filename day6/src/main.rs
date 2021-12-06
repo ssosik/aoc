57e770dbb2425cc27a2c87bb196fbec8d71dc9e0
steve <ssosik@akamai.com>
2021-12-06 10:15:11 -0500

    Day6 part1 done: 355386

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,8 +4,8 @@ use std::io::{BufRead, BufReader};
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 fn main() {
-    let n = 18;
-    //let n = 80;
+    //let n = 18;
+    let n = 80;
     let mut input = BufReader::new(std::io::stdin())
         .lines()
         .next()
@@ -17,13 +17,19 @@ fn main() {
     println!("{:>5}: input {:?}", 0, input);
     for i in 1..=n {
         let mut tmp: Vec<usize> = Vec::new();
+        let mut babies: Vec<usize> = Vec::new();
         for t in input.clone() {
-            tmp.extend(match t {
-                0 => [6],
-                _ => [t-1],
+            tmp.push(match t {
+                0 => {
+                    babies.push(8);
+                    6
+                }
+                _ => t - 1,
             });
         }
         input = tmp;
-        println!("{:>5}: input {:?}", i, input);
+        input.extend(babies);
+        //println!("{:>5}: input {:?}", i, input);
     }
+    println!("Len: {}", input.len());
 }
