27ac8e8e6d81dd8b5cfdad19a61f2b8a77059811
steve <ssosik@akamai.com>
2021-12-06 10:31:59 -0500

    Starting to move to async calculations

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,23 +1,42 @@
-use std::error;
 use std::io::{BufRead, BufReader};
 
-type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
-
 fn main() {
-    //let n = 18;
-    let n = 80;
-    let mut input = BufReader::new(std::io::stdin())
+    let n = 18;
+    let sum: usize = BufReader::new(std::io::stdin())
         .lines()
         .next()
         .unwrap()
         .unwrap()
         .split(',')
-        .filter_map(|v| v.parse::<usize>().ok())
-        .collect::<Vec<usize>>();
-    println!("{:>5}: input {:?}", 0, input);
+        .map(|v| breed(v.parse::<u8>().unwrap(), n))
+        .sum();
+    println!("{:>5}: input {:?}", 0, sum);
+    //for i in 1..=n {
+    //    let mut tmp: Vec<usize> = Vec::new();
+    //    let mut babies: Vec<usize> = Vec::new();
+    //    for t in input.clone() {
+    //        tmp.push(match t {
+    //            0 => {
+    //                babies.push(8);
+    //                6
+    //            }
+    //            _ => t - 1,
+    //        });
+    //    }
+    //    input = tmp;
+    //    input.extend(babies);
+    //    //println!("{:>5}: input {:?}", i, input);
+    //    println!("{}", i);
+    //}
+    //println!("Len: {}", input.len());
+}
+
+fn breed(s: u8, n: u16) -> usize {
+    let mut input = vec![s];
+
     for i in 1..=n {
-        let mut tmp: Vec<usize> = Vec::new();
-        let mut babies: Vec<usize> = Vec::new();
+        let mut tmp: Vec<u8> = Vec::new();
+        let mut babies: Vec<u8> = Vec::new();
         for t in input.clone() {
             tmp.push(match t {
                 0 => {
@@ -30,6 +49,8 @@ fn main() {
         input = tmp;
         input.extend(babies);
         //println!("{:>5}: input {:?}", i, input);
+        println!("{}", i);
     }
-    println!("Len: {}", input.len());
+    //println!("Len: {}", input.len());
+    input.len()
 }
