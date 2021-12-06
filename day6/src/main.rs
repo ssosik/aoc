0a11f75ef0744f3c8b269c1edf2fd4d146e68dee
steve <ssosik@akamai.com>
2021-12-06 10:54:35 -0500

    Async WIP

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,37 +1,28 @@
 use std::io::{BufRead, BufReader};
+use futures::future::try_join_all;
+use futures::Future;
+use std::error;
 
-fn main() {
-    let n = 18;
-    let sum: usize = BufReader::new(std::io::stdin())
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+
+#[tokio::main]
+async fn main() {
+    let n = 80;
+    let input = BufReader::new(std::io::stdin())
         .lines()
         .next()
         .unwrap()
         .unwrap()
         .split(',')
-        .map(|v| breed(v.parse::<u8>().unwrap(), n))
-        .sum();
-    println!("{:>5}: input {:?}", 0, sum);
-    //for i in 1..=n {
-    //    let mut tmp: Vec<usize> = Vec::new();
-    //    let mut babies: Vec<usize> = Vec::new();
-    //    for t in input.clone() {
-    //        tmp.push(match t {
-    //            0 => {
-    //                babies.push(8);
-    //                6
-    //            }
-    //            _ => t - 1,
-    //        });
-    //    }
-    //    input = tmp;
-    //    input.extend(babies);
-    //    //println!("{:>5}: input {:?}", i, input);
-    //    println!("{}", i);
-    //}
-    //println!("Len: {}", input.len());
+        .filter_map(|v| v.parse::<u8>().ok())
+        .collect::<Vec<u8>>();
+    let jobs = input.iter().map(|x| breed(*x, n));
+    let nums: Vec<usize> = try_join_all(jobs).await.unwrap();
+    //let jobs: Vec<dyn Future<Output = usize>> = Vec::with_capacity(100);
+    println!("input {:?} {}", nums, nums.iter().fold(0, |acc, x| x + acc));
 }
 
-fn breed(s: u8, n: u16) -> usize {
+async fn breed(s: u8, n: u16) -> Result<usize> {
     let mut input = vec![s];
 
     for i in 1..=n {
@@ -52,5 +43,5 @@ fn breed(s: u8, n: u16) -> usize {
         println!("{}", i);
     }
     //println!("Len: {}", input.len());
-    input.len()
+    Ok(input.len())
 }
