be0740f077bcb30aa38200a7ce31226f1dfd96fa
steve <ssosik@akamai.com>
2021-12-06 10:12:09 -0500

    WIP

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,7 +4,9 @@ use std::io::{BufRead, BufReader};
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 fn main() {
-    let input = BufReader::new(std::io::stdin())
+    let n = 18;
+    //let n = 80;
+    let mut input = BufReader::new(std::io::stdin())
         .lines()
         .next()
         .unwrap()
@@ -12,5 +14,16 @@ fn main() {
         .split(',')
         .filter_map(|v| v.parse::<usize>().ok())
         .collect::<Vec<usize>>();
-    println!("input {:?}", input);
+    println!("{:>5}: input {:?}", 0, input);
+    for i in 1..=n {
+        let mut tmp: Vec<usize> = Vec::new();
+        for t in input.clone() {
+            tmp.extend(match t {
+                0 => [6],
+                _ => [t-1],
+            });
+        }
+        input = tmp;
+        println!("{:>5}: input {:?}", i, input);
+    }
 }
