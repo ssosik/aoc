dbfcda171e1ebf7cae1b5b57719cffc140226bb6
steve <ssosik@akamai.com>
2021-12-07 09:02:27 -0500

    WIP

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -14,22 +14,27 @@ fn main() {
         .collect();
     println!("positions {:?}", positions);
 
-    let mut costs: BTreeMap<isize, isize> = positions.iter().fold(BTreeMap::new(), |mut acc, x| {
-        acc.insert(*x, 0);
+    let max = positions.iter().max().unwrap();
+    let mut costs: BTreeMap<isize, isize> = (0..=*max).fold(BTreeMap::new(), |mut acc, x| {
+        acc.insert(x, 0);
         acc
     });
-    println!("positions {:?}", costs);
+    println!("costs {:?}", costs);
 
     for pos in positions {
         let mut tmp: BTreeMap<isize, isize> = BTreeMap::new();
         for (cost, cnt) in &costs {
-            //println!("cost {} cnt {} positon {}", cost, cnt, pos);
-            tmp.insert(*cost, cnt + (pos - cost).abs());
+            let sum = (1..=(pos - cost).abs())
+                .enumerate()
+                .inspect(|x| println!("X:{:?}", x))
+                .fold(0, |acc, (_i, p)| acc + p as isize);
+            println!("cost {} cnt {} positon {} sum {}", cost, cnt, pos, sum);
+            tmp.insert(*cost, cnt + sum);
         }
         costs.append(&mut tmp);
     }
 
-    println!("positions {:?}", costs);
+    println!("costs {:?}", costs);
     let min = costs
         .iter()
         .fold(isize::MAX, |acc, (_, v)| cmp::min(acc, *v));
