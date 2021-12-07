84b53b5c7514faa0043ee2b7ef4017187e019ec8
steve <ssosik@akamai.com>
2021-12-07 09:04:51 -0500

    Day 7 part2 done: 99788435

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -3,7 +3,6 @@ use std::collections::BTreeMap;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    println!("Hello, world!");
     let positions: Vec<isize> = BufReader::new(std::io::stdin())
         .lines()
         .next()
@@ -12,23 +11,23 @@ fn main() {
         .split(',')
         .filter_map(|v| v.parse::<isize>().ok())
         .collect();
-    println!("positions {:?}", positions);
+    //println!("positions {:?}", positions);
 
     let max = positions.iter().max().unwrap();
     let mut costs: BTreeMap<isize, isize> = (0..=*max).fold(BTreeMap::new(), |mut acc, x| {
         acc.insert(x, 0);
         acc
     });
-    println!("costs {:?}", costs);
+    //println!("costs {:?}", costs);
 
     for pos in positions {
         let mut tmp: BTreeMap<isize, isize> = BTreeMap::new();
         for (cost, cnt) in &costs {
             let sum = (1..=(pos - cost).abs())
                 .enumerate()
-                .inspect(|x| println!("X:{:?}", x))
+                //.inspect(|x| println!("X:{:?}", x))
                 .fold(0, |acc, (_i, p)| acc + p as isize);
-            println!("cost {} cnt {} positon {} sum {}", cost, cnt, pos, sum);
+            //println!("cost {} cnt {} positon {} sum {}", cost, cnt, pos, sum);
             tmp.insert(*cost, cnt + sum);
         }
         costs.append(&mut tmp);
