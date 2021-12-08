50b9e4d0748b705f637877a7c588a43ef21fa4a1
steve <ssosik@akamai.com>
2021-12-08 10:17:34 -0500

    Day 8 part 1 done

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,39 +1,33 @@
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let entries: Vec<String> = BufReader::new(std::io::stdin())
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap()
-        .split('|')
-        //.filter_map(|v| v.parse::<isize>().ok())
-        .filter_map(|v| Some(v.to_string()))
-        .collect();
-    println!("entries {:?}", entries);
-
-    //for line in input.lines() {
-    //    let (x1, y1, x2, y2) = match line.unwrap().split(" -> ").collect::<Vec<&str>>()[..] {
-    //        [start, end] => {
-    //            let (x1, y1) = match start
-    //                .split(',')
-    //                .filter_map(|v| v.parse::<usize>().ok())
-    //                .collect::<Vec<usize>>()[..]
-    //            {
-    //                [x1, y1] => (x1, y1),
-    //                _ => unreachable!(),
-    //            };
-    //            let (x2, y2) = match end
-    //                .split(',')
-    //                .filter_map(|v| v.parse::<usize>().ok())
-    //                .collect::<Vec<usize>>()[..]
-    //            {
-    //                [x2, y2] => (x2, y2),
-    //                _ => unreachable!(),
-    //            };
-    //            (x1, y1, x2, y2)
-    //        }
-    //        _ => unreachable!(),
-    //    };
-    //};
+    let mut cnt1478 = 0;
+    let input = BufReader::new(std::io::stdin());
+    for line in input.lines() {
+        let (all_digits, display) = match line.unwrap().split(" | ").collect::<Vec<&str>>()[..] {
+            [all, out] => {
+                let (all, out) = (
+                    all.to_string()
+                        .split_whitespace()
+                        .map(|n| n.parse::<String>().ok())
+                        .collect::<Vec<_>>(),
+                    out.to_string()
+                        .split_whitespace()
+                        .map(|n| n.parse::<String>().ok())
+                        .collect::<Vec<_>>(),
+                );
+                cnt1478 += out
+                    .iter()
+                    .map(|e| match e.as_ref().unwrap().len() {
+                        2 | 3 | 4 | 7 => 1,
+                        _ => 0,
+                    })
+                    .sum::<usize>();
+                (all, out)
+            }
+            _ => unreachable!(),
+        };
+        println!("All {:?}; Out {:?}", all_digits, display);
+    }
+    println!("Cnt: {}", cnt1478);
 }
