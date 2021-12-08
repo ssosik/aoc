5c1f8fada9a3a1b297e46c842cdccc488544af0e
steve <ssosik@akamai.com>
2021-12-08 10:01:22 -0500

    WIP

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,39 @@
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let entries: Vec<String> = BufReader::new(std::io::stdin())
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap()
+        .split('|')
+        //.filter_map(|v| v.parse::<isize>().ok())
+        .filter_map(|v| Some(v.to_string()))
+        .collect();
+    println!("entries {:?}", entries);
+
+    //for line in input.lines() {
+    //    let (x1, y1, x2, y2) = match line.unwrap().split(" -> ").collect::<Vec<&str>>()[..] {
+    //        [start, end] => {
+    //            let (x1, y1) = match start
+    //                .split(',')
+    //                .filter_map(|v| v.parse::<usize>().ok())
+    //                .collect::<Vec<usize>>()[..]
+    //            {
+    //                [x1, y1] => (x1, y1),
+    //                _ => unreachable!(),
+    //            };
+    //            let (x2, y2) = match end
+    //                .split(',')
+    //                .filter_map(|v| v.parse::<usize>().ok())
+    //                .collect::<Vec<usize>>()[..]
+    //            {
+    //                [x2, y2] => (x2, y2),
+    //                _ => unreachable!(),
+    //            };
+    //            (x1, y1, x2, y2)
+    //        }
+    //        _ => unreachable!(),
+    //    };
+    //};
 }
