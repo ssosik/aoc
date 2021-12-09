bfe0eeea49f4966a6088a199502bcf147edcae3e
steve <ssosik@akamai.com>
2021-12-09 11:41:08 -0500

    Some refactoring

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -5,9 +5,86 @@ use std::io::{BufRead, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
+#[derive(Debug, Clone)]
+struct Grid(Array2D<u32>);
+
+impl Grid {
+    fn row_max(&self) -> u32 {
+        self.0.num_rows() as u32 - 1
+    }
+
+    fn col_max(&self) -> u32 {
+        self.0.num_columns() as u32 - 1
+    }
+
+    fn get_neighbor_coords(&self, r: u32, c: u32) -> Result<BTreeSet<(u32, u32)>> {
+        let mut neighbors: BTreeSet<(u32, u32)> = BTreeSet::new();
+
+        let row_max = self.row_max();
+        let col_max = self.col_max();
+        //println!("HeightMap {:?} {} {}", self, row_max, col_max);
+
+        match (r, c) {
+            // Anywhere in the middle
+            (y, x) if 0 < x && x < col_max && 0 < y && y < row_max => {
+                neighbors.insert((r - 1, c));
+                neighbors.insert((r + 1, c));
+                neighbors.insert((r, c - 1));
+                neighbors.insert((r, c + 1));
+            }
+            // Along the top row excluding the corners
+            (y, x) if 0 < x && x < col_max && y == 0 => {
+                neighbors.insert((r + 1, c));
+                neighbors.insert((r, c - 1));
+                neighbors.insert((r, c + 1));
+            }
+            // Along the left column excluding the corners
+            (y, x) if x == 0 && 0 < y && y < row_max => {
+                neighbors.insert((r - 1, c));
+                neighbors.insert((r + 1, c));
+                neighbors.insert((r, c + 1));
+            }
+            // Along the bottom row excluding the corners
+            (y, x) if 0 < x && x < col_max && y == row_max => {
+                neighbors.insert((r - 1, c));
+                neighbors.insert((r, c - 1));
+                neighbors.insert((r, c + 1));
+            }
+            // Along the right column excluding the corners
+            (y, x) if x == col_max && 0 < y && y < row_max => {
+                neighbors.insert((r - 1, c));
+                neighbors.insert((r + 1, c));
+                neighbors.insert((r, c - 1));
+            }
+            // Top Left corner
+            (y, x) if x == 0 && y == 0 => {
+                neighbors.insert((r, c + 1));
+                neighbors.insert((r + 1, c));
+            }
+            // Top Right corner
+            (y, x) if x == col_max && y == 0 => {
+                neighbors.insert((r, c - 1));
+                neighbors.insert((r + 1, c));
+            }
+            // Bottom Left corner
+            (y, x) if x == 0 && y == row_max => {
+                neighbors.insert((r, c + 1));
+                neighbors.insert((r - 1, c));
+            }
+            // Bottom Right corner
+            (y, x) if x == col_max && y == row_max => {
+                neighbors.insert((r, c - 1));
+                neighbors.insert((r - 1, c));
+            }
+            _ => unreachable!(),
+        };
+        Ok(neighbors)
+    }
+}
+
 fn main() -> Result<()> {
     println!("Hello, world!");
-    let heightmap = Array2D::from_rows(
+    let heightmap = Grid(Array2D::from_rows(
         &BufReader::new(std::io::stdin())
             .lines()
             .map(|line| {
@@ -18,78 +95,24 @@ fn main() -> Result<()> {
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>(),
-    );
+    ));
     let mut low_points: Vec<u32> = Vec::new();
-    let row_max = heightmap.num_rows() - 1;
-    let col_max = heightmap.num_columns() - 1;
-    println!("HeightMap {:?} {} {}", heightmap, row_max, col_max);
-    for (r, row) in heightmap.rows_iter().enumerate() {
+    for (r, row) in heightmap.0.rows_iter().enumerate() {
         for (c, item) in row.enumerate() {
-            //print!("r{} c{} {:>2}  ", r, c, item);
-            let mut neighbors: BTreeSet<&u32> = BTreeSet::new();
-            match (r, c) {
-                // Anywhere in the middle
-                (y, x) if 0 < x && x < col_max && 0 < y && y < row_max => {
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                }
-                // Along the top row excluding the corners
-                (y, x) if 0 < x && x < col_max && y == 0 => {
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                }
-                // Along the left column excluding the corners
-                (y, x) if x == 0 && 0 < y && y < row_max => {
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                }
-                // Along the bottom row excluding the corners
-                (y, x) if 0 < x && x < col_max && y == row_max => {
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                }
-                // Along the right column excluding the corners
-                (y, x) if x == col_max && 0 < y && y < row_max => {
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                }
-                // Top Left corner
-                (y, x) if x == 0 && y == 0 => {
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                }
-                // Top Right corner
-                (y, x) if x == col_max && y == 0 => {
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                    neighbors.insert(heightmap.get(r + 1, c).unwrap());
-                }
-                // Bottom Left corner
-                (y, x) if x == 0 && y == row_max => {
-                    neighbors.insert(heightmap.get(r, c + 1).unwrap());
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                }
-                // Bottom Right corner
-                (y, x) if x == col_max && y == row_max => {
-                    neighbors.insert(heightmap.get(r, c - 1).unwrap());
-                    neighbors.insert(heightmap.get(r - 1, c).unwrap());
-                }
-                _ => unreachable!(),
-            };
-            //println!("  neighbors {:?}", neighbors);
-            if neighbors
-                .iter().find(|&x| *x <= item).is_none()
+            if !heightmap
+                .get_neighbor_coords(r as u32, c as u32)?
+                .iter()
+                .any(|(x, y)| heightmap.0.get(*x as usize, *y as usize).unwrap() <= item)
             {
                 low_points.push(*item);
             }
         }
     }
-    println!("low_points {:?} {}", low_points, low_points.iter().map(|x| x + 1).sum::<u32>());
+    println!(
+        "low_points {:?} {}",
+        low_points,
+        low_points.iter().map(|x| x + 1).sum::<u32>()
+    );
 
     Ok(())
 }
