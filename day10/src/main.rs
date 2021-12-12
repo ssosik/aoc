c9506e1fabae82a0df7af027cdf829c05e127973
steve <ssosik@akamai.com>
2021-12-11 20:12:52 -0500

    Part 1 done: 369105

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,26 +1,48 @@
+use std::collections::HashMap;
 use std::error;
 use std::io::{BufRead, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 fn main() {
-    println!("Hello, world!");
+    //let point_values = HashMap::from([(')', 3), (']', 57), ('}', 1197), ('>', 25137)]);
+    let point_values = HashMap::from([('(', 1), ('[', 2), ('{', 3), ('<', 4)]);
+
+    let corrupted_sum = 0;
+    let mut valid_lines: Vec<Vec<char>> = Vec::new();
+
     for line in BufReader::new(std::io::stdin()).lines() {
         let mut stack: Vec<char> = Vec::new();
+        let mut line_corrupted = false;
         for char in line.unwrap().chars() {
             match char {
                 '[' | '(' | '{' | '<' => stack.push(char),
-                curr => {
-                    let last = stack.pop().unwrap();
-                    match (last, curr) {
-                        ('{', '}') | ('(', ')') | ('[', ']') | ('<', '>') => {
-                            println!("OK: {} {}", last, curr)
-                        }
-                        _ => println!("NOT OK {} {}", last, curr),
+                curr => match (stack.pop().unwrap(), curr) {
+                    ('{', '}') | ('(', ')') | ('[', ']') | ('<', '>') => {
+                        continue;
                     }
-                }
+                    (_, _curr) => {
+                        // Corrupted line, skip it
+                        //corrupted_sum += point_values[&curr];
+                        line_corrupted = true;
+                        break;
+                    }
+                },
             }
-            println!("{}", char);
         }
+        if !line_corrupted {
+            valid_lines.push(stack);
+        }
+    }
+    println!("Corrupted Sum {}", corrupted_sum);
+    for line in valid_lines {
+        let mut completed_sum: usize = 0;
+        for char in line.iter().rev() {
+            completed_sum *= 5;
+            completed_sum += point_values[char];
+            print!("{}", char);
+        }
+        println!();
+        println!("Completed Sum {}", completed_sum);
     }
 }
