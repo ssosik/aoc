39a49c66aed5877ac56988e3fbda439051873e5a
steve <ssosik@akamai.com>
2021-12-15 15:24:19 -0500

    Finally good progress

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -10,20 +10,38 @@ fn main() {
             _ => unreachable!(),
         }
     }) {
-        println!("a {} b {}", a, b);
-        nodes.entry(a.clone())
+        //println!("a {} b {}", a, b);
+        nodes
+            .entry(a.clone())
             .and_modify(|bset| {
                 bset.insert(b.clone());
-                ()
             })
-            .or_insert(BTreeSet::from([b.clone()]));
-        nodes.entry(b.clone())
+            .or_insert_with(|| BTreeSet::from([b.clone()]));
+        nodes
+            .entry(b.clone())
             .and_modify(|bset| {
                 bset.insert(a.clone());
-                ()
             })
-            .or_insert(BTreeSet::from([a]));
+            .or_insert_with(|| BTreeSet::from([a.clone()]));
     }
-    println!("{:?}", nodes);
+    println!("{:?}", nodes.clone());
+    let result = walk("start".to_string(), nodes.clone(), Vec::new());
+    println!("Result {}", result);
 }
 
+fn walk(curr: String, nodes: BTreeMap<String, BTreeSet<String>>, mut seen: Vec<String>) -> usize {
+    //println!("Curr {} Seen {:?}", curr, seen);
+    if curr == "end" {
+        seen.push(curr);
+        println!("Found end {:?}", seen);
+        1
+    } else if curr.chars().all(char::is_lowercase) && seen.contains(&curr) {
+        //println!("Found double visit for {} in {:?}", curr, seen);
+        0
+    } else {
+        seen.push(curr.clone());
+        nodes.get(&curr).unwrap().iter().fold(0, |acc, next| {
+            acc + walk(next.clone(), nodes.clone(), seen.clone())
+        })
+    }
+}
