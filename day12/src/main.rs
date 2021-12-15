8d5cc0fe6eed08f6f48c0526d94fff1d7b34565b
steve <ssosik@akamai.com>
2021-12-15 15:49:40 -0500

    WIP part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,6 +1,36 @@
 use std::collections::{BTreeMap, BTreeSet};
 use std::io::{BufRead, BufReader};
 
+#[derive(Debug)]
+struct Paths(BTreeSet<Vec<String>>);
+impl Paths {
+    fn new() -> Paths {
+        Paths(BTreeSet::new())
+    }
+
+    fn walk(
+        &mut self,
+        curr: String,
+        nodes: BTreeMap<String, BTreeSet<String>>,
+        mut seen: Vec<String>,
+    ) -> usize {
+        if curr == "end" {
+            seen.push(curr);
+            println!("Found end {:?}", seen);
+            self.0.insert(seen);
+            1
+        } else if curr.chars().all(char::is_lowercase) && seen.contains(&curr) {
+            //println!("Found double visit for {} in {:?}", curr, seen);
+            0
+        } else {
+            seen.push(curr.clone());
+            nodes.get(&curr).unwrap().iter().fold(0, |acc, next| {
+                acc + self.walk(next.clone(), nodes.clone(), seen.clone())
+            })
+        }
+    }
+}
+
 fn main() {
     let mut nodes: BTreeMap<String, BTreeSet<String>> = BTreeMap::new();
     for (a, b) in BufReader::new(std::io::stdin()).lines().map(|line| {
@@ -25,23 +55,7 @@ fn main() {
             .or_insert_with(|| BTreeSet::from([a.clone()]));
     }
     println!("{:?}", nodes.clone());
-    let result = walk("start".to_string(), nodes.clone(), Vec::new());
-    println!("Result {}", result);
-}
-
-fn walk(curr: String, nodes: BTreeMap<String, BTreeSet<String>>, mut seen: Vec<String>) -> usize {
-    //println!("Curr {} Seen {:?}", curr, seen);
-    if curr == "end" {
-        seen.push(curr);
-        println!("Found end {:?}", seen);
-        1
-    } else if curr.chars().all(char::is_lowercase) && seen.contains(&curr) {
-        //println!("Found double visit for {} in {:?}", curr, seen);
-        0
-    } else {
-        seen.push(curr.clone());
-        nodes.get(&curr).unwrap().iter().fold(0, |acc, next| {
-            acc + walk(next.clone(), nodes.clone(), seen.clone())
-        })
-    }
+    let mut paths = Paths::new();
+    paths.walk("start".to_string(), nodes.clone(), Vec::new());
+    println!("Result {:?}", paths.0.len());
 }
