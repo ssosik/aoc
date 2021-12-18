c283efce35414403740267bcfd1283f34889328c
steve <ssosik@akamai.com>
2021-12-18 16:24:37 -0500

    WIP

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -9,21 +9,25 @@ use std::str::FromStr;
 struct Instructions(u8, BTreeMap<(char, char), char>);
 
 impl Instructions {
-    fn recurse(&self, depth: u8, left: char, right: char) -> Vec<char> {
-        println!("Here {} left {} right {}", depth, left, right);
-        //let mut ret = vec!(left);
+    fn recurse(&self, left: char, right: char) -> Vec<char> {
+        let mut ret = vec![left];
+        ret.extend(self._recurse(0, left, right));
+        ret
+    }
+    fn _recurse(&self, depth: u8, left: char, right: char) -> Vec<char> {
+        //println!("Here {} left {} right {}", depth, left, right);
         let mut ret = Vec::new();
         if depth >= self.0 {
-            return vec!(right);
+            return vec![right];
         }
         match self.1.get(&(left, right)) {
             Some(val) => {
-                ret.extend(self.recurse(depth + 1, left, *val));
-                ret.extend(self.recurse(depth + 1, *val, right));
-            },
+                ret.extend(self._recurse(depth + 1, left, *val));
+                ret.extend(self._recurse(depth + 1, *val, right));
+            }
             None => {
                 println!("NONE {} left {} right {}", depth, left, right);
-                return vec!(left, right);
+                return vec![left, right];
             }
         };
         ret
@@ -33,7 +37,8 @@ impl Instructions {
 fn main() {
     let lines: Vec<_> = BufReader::new(std::io::stdin()).lines().collect();
 
-    let mut input = lines.get(0)
+    let mut input = lines
+        .get(0)
         .unwrap()
         .as_ref()
         .unwrap()
@@ -57,43 +62,28 @@ fn main() {
             _ => unreachable!("EEk {:?}", line),
         }
     }
-    let instructions = Instructions(4, instructions);
+
+    let instructions = Instructions(10, instructions);
     println!("Instructions {:?}", instructions);
-    let ret = instructions.recurse(0, input[0], input[1]);
-    println!("RET {:?}", ret.into_iter().collect::<String>());
+    //let ret = instructions.recurse(input[0], input[1]);
+    //println!("RET {:?}", ret.into_iter().collect::<String>());
 
-    //for i in 0..40 {
-    //    let mut tmp = Vec::new();
-    //    let mut last: char = 'X';
-    //    for tuple in template
-    //        .iter()
-    //        .tuple_windows::<(_, _)>()
-    //        .collect::<Vec<_>>()
-    //    {
-    //        let tuple = (*tuple.0, *tuple.1);
-    //        //println!("Tuple {:?} {:?}", tuple, instructions.get(&tuple));
-    //        tmp.push(tuple.0);
-    //        if let Some(c) = instructions.get(&tuple) {
-    //            tmp.push(*c);
-    //        }
-    //        last = tuple.1;
-    //        //print!(".");
-    //    }
-    //    tmp.push(last);
-    //    template = tmp;
-    //    //println!("Template {:?}", template);
-    //    println!("Loop {}", i);
-    //}
+    let mut counts: BTreeMap<char, usize> = BTreeMap::new();
+    for tuple in input.iter().tuple_windows::<(_, _)>().collect::<Vec<_>>() {
+        println!("Chunk {}{}", *tuple.0, *tuple.1);
+        for c in instructions.recurse(*tuple.0, *tuple.1) {
+            counts.entry(c).and_modify(|x| *x += 1).or_insert(1);
+        }
+    }
 
-    //let mut counts: BTreeMap<char, usize> = BTreeMap::new();
-    //for c in template {
-    //    counts.entry(c).and_modify(|x| *x += 1).or_insert(1);
-    //}
+    let mut values: Vec<usize> = counts.into_values().collect();
+    values.sort_unstable();
+    println!(
+        "Values: {:?} min {} max {}",
+        values,
+        values[0],
+        values[values.len() - 1]
+    );
 
-    //let mut values: Vec<usize> = counts.into_values().collect();
-    //values.sort_unstable();
-    //println!("Values: {:?} min {} max {}", values, values[0], values[values.len() - 1]);
-
-
-    //println!("Score: {}", values[values.len() - 1] - values[0]);
+    println!("Score: {}", values[values.len() - 1] - values[0]);
 }
