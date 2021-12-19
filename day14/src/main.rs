3a9553ad454e9e59a3b14962b5b5c79dbf12c4de
steve <ssosik@akamai.com>
2021-12-19 13:51:08 -0500

    WIP refactoring to speed things up checkpoint

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,36 +1,78 @@
-#![feature(iter_map_while)]
 use itertools::Itertools;
 use std::collections::BTreeMap;
-
+use std::fmt;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
 
 #[derive(Debug)]
-struct Instructions(u8, BTreeMap<(char, char), char>);
+struct Instructions {
+    depth: u8,
+    instructions: BTreeMap<(char, char), char>,
+    counts: BTreeMap<char, usize>,
+}
 
-impl Instructions {
-    fn recurse(&self, left: char, right: char) -> Vec<char> {
-        let mut ret = vec![left];
-        ret.extend(self._recurse(0, left, right));
-        ret
-    }
-    fn _recurse(&self, depth: u8, left: char, right: char) -> Vec<char> {
-        println!("Here {} left {} right {}", depth, left, right);
-        let mut ret = Vec::new();
-        if depth >= self.0 {
-            return vec![right];
-        }
-        match self.1.get(&(left, right)) {
-            Some(val) => {
-                ret.extend(self._recurse(depth + 1, left, *val));
-                ret.extend(self._recurse(depth + 1, *val, right));
-            }
-            None => {
-                println!("NONE {} left {} right {}", depth, left, right);
-                return vec![left, right];
-            }
+enum Instruction {
+    Counted(char),
+    Uncounted(char),
+}
+
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Instruction::Counted(c) => write!(f, "Counted {}", c),
+            Instruction::Uncounted(c) => write!(f, "Uncounted {}", c),
         };
-        ret
+        Ok(())
+    }
+}
+impl Instructions {
+    fn new(depth: u8) -> Instructions {
+        Instructions {
+            depth: depth,
+            instructions: BTreeMap::new(),
+            counts: BTreeMap::new(),
+        }
+    }
+    fn insert(&mut self, left: char, right: char, insert: char) {
+        self.instructions.insert((left, right), insert);
+    }
+    fn get(&self, left: Instruction, right: Instruction) -> Option<&char> {
+        let left = match left {
+            Instruction::Counted(x) => x,
+            Instruction::Uncounted(x) => x,
+        };
+        let right = match right {
+            Instruction::Counted(x) => x,
+            Instruction::Uncounted(x) => x,
+        };
+        self.instructions.get(&(left, right))
+    }
+    fn count(&mut self, c: Instruction) -> Instruction {
+        match c {
+            Instruction::Counted(x) => c,
+            Instruction::Uncounted(x) => {
+                self.counts.entry(x).and_modify(|x| *x += 1).or_insert(1);
+                Instruction::Counted(x)
+            }
+        }
+    }
+    fn recurse(&self, left: char, right: char) {
+        self._recurse(
+            0,
+            Instruction::Uncounted(left),
+            Instruction::Uncounted(right),
+        )
+    }
+    fn _recurse(&self, depth: u8, left: Instruction, right: Instruction) {
+        println!("Here {} left {} right {}", depth, left, right);
+        if depth < self.depth {
+            let left = self.count(left);
+            let right = self.count(right);
+            if let Some(val) = self.get(left, right) {
+                self._recurse(depth + 1, left, Instruction::Uncounted(*val));
+                self._recurse(depth + 1, Instruction::Uncounted(*val), right);
+            }
+        }
     }
 }
 
@@ -47,14 +89,14 @@ fn main() {
     println!("input {:?}", input);
 
     // Load the initial coordinate inputs
-    let mut instructions: BTreeMap<(char, char), char> = BTreeMap::new();
+    let mut instructions = Instructions::new(10);
     for line in lines.iter().skip(2) {
         // Split each line on ',' character, passing the two pieces into the match
         match line.as_ref().unwrap().split(" -> ").collect::<Vec<&str>>()[..] {
             [a, b] => {
                 match a.chars().into_iter().collect::<Vec<char>>()[..] {
                     [x, y] => {
-                        instructions.insert((x, y), char::from_str(b).unwrap());
+                        instructions.insert(x, y, char::from_str(b).unwrap());
                     }
                     _ => unreachable!("EEk {:?}", line),
                 };
@@ -63,20 +105,16 @@ fn main() {
         }
     }
 
-    let instructions = Instructions(10, instructions);
     println!("Instructions {:?}", instructions);
     //let ret = instructions.recurse(input[0], input[1]);
     //println!("RET {:?}", ret.into_iter().collect::<String>());
 
-    let mut counts: BTreeMap<char, usize> = BTreeMap::new();
     for tuple in input.iter().tuple_windows::<(_, _)>().collect::<Vec<_>>() {
         println!("Chunk {}{}", *tuple.0, *tuple.1);
-        for c in instructions.recurse(*tuple.0, *tuple.1) {
-            counts.entry(c).and_modify(|x| *x += 1).or_insert(1);
-        }
+        instructions.recurse(*tuple.0, *tuple.1);
     }
 
-    let mut values: Vec<usize> = counts.into_values().collect();
+    let mut values: Vec<usize> = instructions.counts.into_values().collect();
     values.sort_unstable();
     println!(
         "Values: {:?} min {} max {}",
