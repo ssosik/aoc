75fdc9b41f53d572dfe1fee205866a59344b253e
steve <ssosik@akamai.com>
2021-12-20 14:52:10 -0500

    Deserializing inputs

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,17 @@
+use array2d::Array2D;
+use std::cmp;
+use std::fmt;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<Vec<u32>> = BufReader::new(std::io::stdin())
+        .lines()
+        //.map(|l| l.unwrap().chars().map(|c| c.to_digit(10_u32).unwrap()))
+        .map(|l| l.unwrap().chars().collect::<_>())
+        .map(|l: Vec<char>| l.iter().map(|c| c.to_digit(10_u32).unwrap()).collect::<Vec<u32>>())
+        .inspect(|x| println!("X {:?}", x))
+        .collect();
+    let lines = Array2D::from_rows(&lines);
+
+    println!("lines: {:?}", lines);
 }
