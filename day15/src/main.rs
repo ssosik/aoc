3c4b2ce85118075523a41b9662a331f9875f14b9
steve <ssosik@akamai.com>
2021-12-21 14:35:54 -0500

    WIP

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,8 +1,17 @@
 use array2d::Array2D;
-use pathfinding::prelude::*;
+use pathfinding::prelude::{absdiff, astar};
 use std::cmp;
 use std::fmt;
 use std::io::{BufRead, BufReader};
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(usize, usize, u32);
+
+impl Pos {
+    fn distance(&self, other: &Pos) -> u32 {
+        (absdiff(self.0, other.0) + absdiff(self.1, other.1)) as u32
+    }
+}
 
 #[derive(Debug, Clone)]
 struct Grid(Array2D<u32>);
@@ -10,6 +19,21 @@ struct Grid(Array2D<u32>);
 impl Grid {
     fn new(lines: &Vec<Vec<u32>>) -> Grid {
         Grid(Array2D::from_rows(lines))
+    }
+    fn neighbors(&self, p: &Pos) -> Vec<(Pos, u32)> {
+            let &Pos(x, y, _) = p;
+            println!("HERE {} {}", x, y);
+            //vec![Pos(x+1,y+2), Pos(x+1,y-2), Pos(x-1,y+2), Pos(x-1,y-2),
+            //     Pos(x+2,y+1), Pos(x+2,y-1), Pos(x-2,y+1), Pos(x-2,y-1)]
+            //     .into_iter().map(|p| (p, 1)).collect()
+            match (x, y) {
+                (0, 0) => println!("upper left"),
+                (0, 1) => println!("upper right"),
+                (1, 0) => println!("lower left"),
+                (1, 1) => println!("lower right"),
+                (a, b) => println!("mid {} {}", a, b),
+            };
+            vec![(Pos(1, 2, 3), 4)]
     }
 }
 
@@ -29,7 +53,22 @@ fn main() {
     let rows = grid.0.num_rows();
     let columns = grid.0.num_columns();
 
+    let start = Pos(0, 0, *grid.0.get(0, 0).unwrap());
+    let goal: Pos = Pos(
+        rows - 1,
+        columns - 1,
+        *grid.0.get(rows - 1, columns - 1).unwrap(),
+    );
+
     println!("lines: {:?}", lines);
     println!("rows: {} columns {}", rows, columns);
     println!("goal {}", grid.0.get(rows - 1, columns - 1).unwrap());
+
+    let result = astar(&start,
+        |p| grid.neighbors(p),
+        |p| p.distance(&goal) / 3,
+        |p| *p == goal
+        );
+    println!("result: {:?}", result);
+    
 }
